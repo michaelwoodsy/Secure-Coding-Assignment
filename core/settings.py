673d667da61575eef2f7b77c0ba21bc8aaf3eccc
Joseph Kelly <jke99@uclive.ac.nz>
2022-09-29 16:22:12 +1300

    1.13 session expiry fixed

--- core/settings.py
+++ core/settings.py
@@ -64,7 +64,17 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
+
+# Session expiry
+
+SESSION_EXPIRE_SECONDS = 3600  # 1 hour
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = '/'
+
+
+
 
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.PBKDF2PasswordHasher",
